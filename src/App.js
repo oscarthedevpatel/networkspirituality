c831e84d35522071ec51a75e44b33c4cb159a34c
oscar <jesserjacob99@gmail.com>
2023-11-13 00:19:40 -0500

    contract

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import "./App.css";
-import React, { Component, createRef } from "react";
-import Navigation from "./components/Navbar/MyNavbar";
-import FallingImages from "./components/FallingImages/FallingImages";
-import CentralNav from "./components/CentralNav/centralNav"
+import React, { Component } from "react";
+import CentralNav from "./components/CentralNav/centralNav";
 
 class App extends Component {
   render() {
     return (
       <div className="background">
-        {/* <FallingImages /> */}
-        <CentralNav />
+        <div className="column-container">
+          {/* <FallingImages /> */}
+          <CentralNav />
+          <br></br>
+          <br></br>
 
+          <br></br>
+          <br></br>
+          
 
+          <div className="contract">
+            0x2182014c324d57F7aF6A37a1C2Db11dac588F58A
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 export default App;
